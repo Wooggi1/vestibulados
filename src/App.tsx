ce06f8e9b595dd550cfe570b30a4fb133e53a089
Punkrig <igorbrandaao@gmail.com>
2024-09-27 10:22:18 -0300

    Desenvolvimento: Página de layot              - Finalizando o desenvolvimento da página de layout

--- src/App.tsx
+++ src/App.tsx
@@ -10,14 +10,18 @@ function App() {
       <Header/>
       <div className="container">
         <div className='text-content'>
-        <h1 className="mainText">Treine questões com amigos</h1>
-        <p>Vestibulandos é um app que  permite que 
-          você que estuda para concurso eu vestibular consiga 
-          treinar questões de maneira 
-          didática e gameficada com seus amigos.</p>
+          <h1 className="mainText">Treine questões com amigos</h1>
+          <p>Vestibulandos é um app que  permite que 
+            você que estuda para concurso eu vestibular consiga 
+            treinar questões de maneira 
+            didática e gameficada com seus amigos.
+          </p>
+          <button className='button-saibaMais'>Saiba mais</button>
+        </div>
+        <div className="img-container">
+          <img className="main-img" src='/images/img-students.jpg'/>
         </div>
       </div>
-      <img className="main-img" src='/images/img-students.jpg'/>
     </div>
 
   
